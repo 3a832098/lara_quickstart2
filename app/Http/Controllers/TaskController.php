20574f6100dc018667fce7cb127937ddd3f37be9
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-25 17:40:07 +0800

    顯示任務，使用DB擷取使用者所有任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -14,7 +15,8 @@ class TaskController extends Controller
 
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks=Task::where('user_id',$request->user()->id)->get();
+        return view('tasks.index',['tasks'=>$tasks]);
     }
 
     public function store(Request $request)
